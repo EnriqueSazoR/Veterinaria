31369ad88d174c633b4e878cb2806259e800540f
EnriqueSazoR <luisrosales9598@gmail.com>
2025-04-17 18:47:15 -0600

    Configuración Incial (Se trabajará por modulos, capas y areas)

---
+++ Veterinaria.AccesoDatos/Data/Repository/ContenedorTrabajo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Veterinaria.AccesoDatos.Data.Repository.IRepository;
+using Veterinaria.Data;
+
+namespace Veterinaria.AccesoDatos.Data.Repository
+{
+    public class ContenedorTrabajo : IContenedorTrabajo
+    {
+
+        private readonly ApplicationDbContext _db;
+
+        public ContenedorTrabajo(ApplicationDbContext db)
+        {
+            _db = db;
+            // Se llaman a todos los repositorios que quedan encapsulados
+
+        }
+        public void Dispose()
+        {
+            _db.Dispose();
+        }
+
+        public void Save()
+        {
+            _db.SaveChanges();
+        }
+    }
+}
---
+++ Veterinaria.AccesoDatos/Data/Repository/IRepository/IContenedorTrabajo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Veterinaria.AccesoDatos.Data.Repository.IRepository
+{
+    public interface IContenedorTrabajo : IDisposable
+    {
+        // Se agregan los repositorios a utlizar en el proyecto
+
+
+
+
+        void Save();
+    }
+}
